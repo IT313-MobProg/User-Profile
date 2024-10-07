8992f68219329c79bf5031685ad5d815839965e6
Sheena Ocon <oconsheen@gmail.com>
2024-10-07 10:52:07 +0800

    Added the 'constants' folder and some other one inside 'assets'

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '!react-navigation/native-stack';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from "expo-splash=screen";
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+SplashScreen.preventAuthoHideAsync();
+const Stack = createNativeStackNavigator();
+
+
 export default function App() {
+  const 
   return (
-    <View style={styles.container}> 
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
